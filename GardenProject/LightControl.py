c895b0502d097851b993d83a845e0be60956f56c
CookieD93 <dani9035@edu.easj.dk>
2018-02-09 14:33:28 +0100

    Update LightControl.py
    Updated Code to accommodate MQTTControl

--- GardenProject/LightControl.py
+++ GardenProject/LightControl.py
@@ -1,17 +1,15 @@
 from RPi import GPIO
-#from MQTTControl import overlord
 GPIO.cleanup()
 
 class Lights:
-    def __init__(self,pinname):
-        self.name = pinname
-        #overlord.register_minion(self)
-    def __str__(self):
-        return self.name
-    def notify(self, command):
-        if (command == "Lights OFF"):
+    def __init__(self,overlord, pinname):
+        self.pinname = pinname
+        overlord.register_minion(self)
+    def notify (self, command):
+        if (command.lower() == "Lights OFF"):
             GPIO.OUTPUT(self.pinname, 0)
-        elif (command == "Lights ON"):
+            "Lights turned off"
+        elif (command.lower() == "Lights ON"):
             GPIO.OUTPUT(self.pinname, 1)
         else:
             return "Unknown Command"
