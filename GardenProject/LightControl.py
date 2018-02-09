0f3ea7b5524b2f08de9fd0d555878783b681081c
CookieD93 <dani9035@edu.easj.dk>
2018-02-09 14:55:33 +0100

    Update LightControl.py
    Fixed some GPIO

--- GardenProject/LightControl.py
+++ GardenProject/LightControl.py
@@ -1,16 +1,17 @@
 from RPi import GPIO
 GPIO.cleanup()
-
+GPIO.setmode(GPIO.BCM)
 class Lights:
     def __init__(self,overlord, pinname):
         self.pinname = pinname
+        GPIO.setup(pinname, GPIO.OUT)
         overlord.register_minion(self)
     def notify (self, command):
         if (command.lower() == "lights off"):
-            GPIO.OUTPUT(self.pinname, 0)
-            print("Lights turned off")
+            GPIO.output(self.pinname, 0)
+            print('Lights turned off')
         elif (command.lower() == "lights on"):
-            GPIO.OUTPUT(self.pinname, 1)
+            GPIO.output(self.pinname, 1)
             print("Lights turned on")
         else:
             return "Unknown Command"
