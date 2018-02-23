c631248339b4102191a5f700b35d7129916bf09f
rebelricco <riccojorgensen@gmail.com>
2018-02-23 12:09:25 +0100

    Update PumpControl2.py

--- GardenProject/PumpControl2.py
+++ GardenProject/PumpControl2.py
@@ -1,6 +1,6 @@
 """
-Created by Rasmus Edin Thomasen (Github: https://github.com/Gitgest)
-           Nicki Feldt (Github: https://github.com/Lardsoup)
+Created by Rasmus Edin Thomasen     (Github: https://github.com/Gitgest)
+           Nicki Feldt              (Github: https://github.com/Lardsoup)
 
 A class meant to control the water-pump.
 Water should be checked every hour.
@@ -8,39 +8,35 @@ Should turn off pump on an off string received.
 Should turn on pump on a on string received.
 """
 from RPi import GPIO
-import time
+import MQTT.WaterMeasurer as WaterSensor
 
 
 class Pump(object):
 
     """ Initializer """
-    def __init__(self, overlord, pinname, Uppermeasure):
+    def __init__(self, overlord, pinname, pinSensor):
         self.pinname = pinname
         GPIO.setup(pinname, GPIO.OUT)
         overlord.register_minion(self)
-        self.LocalUppermeasure = Uppermeasure
-
-
+        self.pinSensor = pinSensor
 
     #Notify method should take a command from the MQTT server.
     #Depending on the command it should either turn the pump on or off.
     #Command should also print on or off message to console.
     #(Command should originate from the humidity measurer.)
-
-
-
     def notify(self, command):
         """ Turn on and off output to relay, returns string based on input. """
         if (command.lower() == "pump on"):
-            waterSensorCheck = 0
+            waterSensorCheck = False
 
-            while(waterSensorCheck == 0):
-                waterSensorCheck = 1
-                waterSensorCheck = self.LocalUppermeasure.PumpCheck()
-                if(waterSensorCheck == 0):
+            while(waterSensorCheck == False):
+                waterSensorCheck = True
+                waterSensorCheck = WaterSensor.PumpCheck(self.pinSensor)
+                if(waterSensorCheck == False):
                     GPIO.output(self.pinname, 1)
                     print('pump turned on')
                 else:
                     GPIO.output(self.pinname, 0)
                     print('pump turned off')
-                time.sleep(0.5)
+
+        return "pump did its thing -Lardador 2018"
