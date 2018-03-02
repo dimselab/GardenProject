098d2d85b13573698561322b03738b2e90e72804
CookieD93 <dani9035@edu.easj.dk>
2018-03-02 11:20:10 +0100

    Update LightControl.py
    Added getStatus to allow the main to see if the light is on or off

--- GardenProject/LightControl.py
+++ GardenProject/LightControl.py
@@ -2,7 +2,6 @@
 Daniel
 Philip
 Niklas
-
 This module is used to act like a light switch.
 it takes a string in the "notify" and turns the light on or off accordingly.
 It is initialised by giving it an overlord (the observer that keeps tracks)
@@ -11,7 +10,10 @@ Calling the notify function needs a "command" and it respectively turns on or of
 prints in console what it did and returns it for functions that are waiting for it.
 """
 from RPi import GPIO
+
 class Lights:
+    isOn = 0;
+     
     def __init__(self,overlord, pinnumber):
         self.pinnumber = pinnumber
         GPIO.setup(pinnumber, GPIO.OUT)
@@ -20,10 +22,15 @@ class Lights:
         if (command.lower() == "lights off"):
             GPIO.output(self.pinnumber, 0)
             print( 'Lights turned off')
+            self.isOn = 0;
             return 'Lights turned off'
         elif (command.lower() == "lights on"):
             GPIO.output(self.pinnumber, 1)
             print( "Lights turned on")
+            self.isOn = 1;
             return "Lights turned on"
         else:
-            return "Unknown Command"
+            return "Unknown Command";
+    def getStatus (self):
+        return self.isOn;
+
