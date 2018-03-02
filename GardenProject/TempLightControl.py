8201210d0becbeb6db08ecebed4682b50ff0a1ff
Anne Sofie Wanscher <anne55x9@edu.easj.dk>
2018-03-02 10:21:36 +0100

    Update TempLightControl.py

--- GardenProject/TempLightControl.py
+++ GardenProject/TempLightControl.py
@@ -13,10 +13,12 @@ import requests
 
 
 class Temperature:
-    def _init_(self,overlord):
-        GPIO.setup(14, GPIO.OUT)
-        GPIO.setup(15, GPIO.OUT)
-        GPIO.setup(18, GPIO.OUT)
+    def _init_(self,overlord, pinnumber1, pinnumber2, pinnumber3):
+        
+        
+        GPIO.setup(pinnumber1, GPIO.OUT)
+        GPIO.setup(pinnumber2, GPIO.OUT)
+        GPIO.setup(pinnumber3, GPIO.OUT)
         overlord.register_minion(self)
     def notify(self, command):
         while (True):
@@ -29,31 +31,31 @@ class Temperature:
 
                 if (temperature < 21):
 
-                    GPIO.output(14, GPIO.LOW)
-                    GPIO.output(15, GPIO.LOW)
-                    GPIO.output(18, GPIO.HIGH)
+                    GPIO.output(pinnumber1, GPIO.LOW)
+                    GPIO.output(pinnumber2, GPIO.LOW)
+                    GPIO.output(pinnumber3, GPIO.HIGH)
 
                 elif (temperature > 29):
 
-                    GPIO.output(14, GPIO.HIGH)
-                    GPIO.output(15, GPIO.LOW)
-                    GPIO.output(18, GPIO.LOW)
+                    GPIO.output(pinnumber1, GPIO.HIGH)
+                    GPIO.output(pinnumber2, GPIO.LOW)
+                    GPIO.output(pinnumber3, GPIO.LOW)
 
                 else:
 
-                    GPIO.output(14, GPIO.LOW)
-                    GPIO.output(15, GPIO.HIGH)
-                    GPIO.output(18, GPIO.LOW)
+                    GPIO.output(pinnumber1, GPIO.LOW)
+                    GPIO.output(pinnumber2, GPIO.HIGH)
+                    GPIO.output(pinnumber3, GPIO.LOW)
 
             url = 'http://garden-project-web-api.herokuapp.com/api/temperatures'
             post_data = {"temperature": "{0:0.2f}".format(float(temperature)),
                          "humidity": "{0:0.2f}".format(float(humidity))}
 
-            # data_json = json.dumps(data)
+          
             headers = {'Content-type': 'application/json'}
             response = requests.post(url, json=post_data, headers=headers)
 
-            # print("\r\nHOURLY\r\nTemp : {0} \r\nStatus : {1} ".format(float(t11), response.status_code))
+            
 
             print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity)
 
