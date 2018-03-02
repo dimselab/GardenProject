71fcf7325f2a18bea6f37802351754d8dc0297a4
Anne Sofie Wanscher <anne55x9@edu.easj.dk>
2018-03-02 10:12:58 +0100

    Update TempLightControl.py

--- GardenProject/TempLightControl.py
+++ GardenProject/TempLightControl.py
@@ -1,55 +1,62 @@
-import sys
+
+
+# Adafruit_DHT library imported to run DHT22 and DHT11 temperature and humidity sensors.
+
+#Definition init sets the used pins in the temperature control method. 
+#Definition notify is the method that measures temperature and depending on the temp the RGB lamp light up blue, green or red. 
+
+
 import Adafruit_DHT as dht
-import time
-import RPi.GPIO as GPIO
-import datetime
+from RPi import GPIO
 import json
 import requests
 
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(14, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
+class Temperature:
+    def _init_(self,overlord):
+        GPIO.setup(14, GPIO.OUT)
+        GPIO.setup(15, GPIO.OUT)
+        GPIO.setup(18, GPIO.OUT)
+        overlord.register_minion(self)
+    def notify(self, command):
+        while (True):
 
-while(True):
+            timer = time.time()
 
-    timer = time.time()
+            while (time.time() - timer < 30):
 
-    while(time.time() - timer < 30):
+                humidity, temperature = dht.read_retry(dht.DHT11, 22)
 
+                if (temperature < 21):
 
-        humidity, temperature = dht.read_retry(dht.DHT11, 22)
+                    GPIO.output(14, GPIO.LOW)
+                    GPIO.output(15, GPIO.LOW)
+                    GPIO.output(18, GPIO.HIGH)
 
-        if(temperature < 21):
+                elif (temperature > 29):
 
-            GPIO.output(14, GPIO.LOW)
-            GPIO.output(15, GPIO.LOW)
-            GPIO.output(18, GPIO.HIGH)
+                    GPIO.output(14, GPIO.HIGH)
+                    GPIO.output(15, GPIO.LOW)
+                    GPIO.output(18, GPIO.LOW)
 
-        elif(temperature > 29):
+                else:
 
-            GPIO.output(14, GPIO.HIGH)
-            GPIO.output(15, GPIO.LOW)
-            GPIO.output(18, GPIO.LOW)
+                    GPIO.output(14, GPIO.LOW)
+                    GPIO.output(15, GPIO.HIGH)
+                    GPIO.output(18, GPIO.LOW)
 
-        else:
+            url = 'http://garden-project-web-api.herokuapp.com/api/temperatures'
+            post_data = {"temperature": "{0:0.2f}".format(float(temperature)),
+                         "humidity": "{0:0.2f}".format(float(humidity))}
 
-            GPIO.output(14, GPIO.LOW)
-            GPIO.output(15, GPIO.HIGH)
-            GPIO.output(18, GPIO.LOW)
+            # data_json = json.dumps(data)
+            headers = {'Content-type': 'application/json'}
+            response = requests.post(url, json=post_data, headers=headers)
 
-    url = 'http://garden-project-web-api.herokuapp.com/api/temperatures'
-    post_data = {"temperature": "{0:0.2f}".format(float(temperature)),"humidity": "{0:0.2f}".format(float(humidity))}
+            # print("\r\nHOURLY\r\nTemp : {0} \r\nStatus : {1} ".format(float(t11), response.status_code))
 
-
-    #data_json = json.dumps(data)
-    headers = {'Content-type': 'application/json'}
-    response = requests.post(url, json=post_data, headers=headers)
-
-    #print("\r\nHOURLY\r\nTemp : {0} \r\nStatus : {1} ".format(float(t11), response.status_code))
-
-    print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity)
+            print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity)
 
 
-GPIO.cleanup()
+
+
